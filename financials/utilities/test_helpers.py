7b63c3b8202594caf7d115d881b8cc9b8da7b108
UD <uberdeveloper001@gmail.com>
2014-01-13 14:10:28 +0530

    test test_helpers

---
+++ financials/utilities/test_helpers.py
@@ -0,0 +1,41 @@
+import helpers
+from pandas import DataFrame
+from numpy import *
+import unittest
+ 
+
+def isDataFramesEqual(*args):
+    # compare whether dataframes are identical
+    for (i,df) in enumerate(args[1:]):
+        df1 = df.sort_index(axis = 1)
+        df2 = args[i].sort_index(axis = 1)
+        try:
+            if not all(df1 == df2):
+                return False
+        except:
+            return False
+    return True
+  
+ 
+def test_DictToDataFrame():
+    dct = {'A':range(4), 'B':10, 'C':'Q', 'D': [10,20], 'E': [24,31,42]}
+    # Initialize data
+    df = DataFrame(range(4), columns = ["A"])
+    df['B'] = 10
+    df['C'] = "Q"
+    df['D'] = Series([10,20])
+    df['E'] = Series([24,31,42])
+    # Run tests
+    
+    assert isDataFramesEqual(df[:2], dict_to_dataframe(dct))
+    assert isDataFramesEqual(df, dict_to_dataframe(dct, rows = "H"))
+    assert isDataFramesEqual(df[:4], dict_to_dataframe(dct, rows = 4))
+    df.loc[[2,3,4],'D'] = [10,20,10] # Changed data to test cycle
+    df.loc[[3,4], 'E'] = [24,31]
+    assert isDataFramesEqual(df[:2], dict_to_dataframe(dct, cycle = True))
+    assert isDataFramesEqual(df, dict_to_dataframe(dct, rows = "H", cycle = True))
+    assert isDataFramesEqual(df[:4], dict_to_dataframe(dct, rows = 4, cycle = True))
+    # TO DO: Test for all as arrays and all input as scalars, rows integer equal to minimum length
+    
+
+
