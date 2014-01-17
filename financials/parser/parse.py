450027b275c0a451b0b741e79ac0ef9052eec181
UD <uberdeveloper001@gmail.com>
2014-01-17 22:22:17 +0530

    module parse and test_parse updated

--- financials/parser/parse.py
+++ financials/parser/parse.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from operator import add,sub,mul,pow,div,floordiv
+from operator import add,sub,mul,pow,div,floordiv,mod
 from operator import lt,le,eq,ne,gt,ge,and_,or_
 
 def _check_char(string, chars):
@@ -21,9 +21,9 @@ def _convert(df, string):
     if type(string) in (str,float,int):
         try:
             r = float(string)
-            return [r]
+            return r
         except ValueError:
-            return df[string]
+            return df[string] if string in df.columns else string
     else:
         return string  
         
@@ -36,7 +36,7 @@ def _is_reduced(sequence):
     return True
         
 
-class Parser(object):
+class SimpleParser(object):
     """
     A Simple Parser class for doing operations on
     dataframes
@@ -48,19 +48,17 @@ class Parser(object):
         """
         self._df = dataframe
         self._expression = expression
-        self._chars = ["+", "-", "*", "/", ">", "<", "=", "&", "|"]
+        self._chars = ["+", "-", "*", "/", ">", "<", "=", "&", "|", "%"]
         self._d = {'+': add, '-': sub, '*': mul, '**': pow, '/': div,
                    '//': floordiv, '<': lt, '<=': le, '==': eq, '!=': ne,
-                   '>': gt, '>=': ge, '&': and_, '|': or_}
+                   '>': gt, '>=': ge, '&': and_, '|': or_, '%': mod}
                    
     def _get_expression(self, expression = None):
         # Assign the default expression if there is None
         if expression is None:
             return self._expression
         else:
-            return expression
-    
-               
+            return expression               
         
         
     def dataframe(self, dataframe):
@@ -93,61 +91,22 @@ class Parser(object):
                     buffers = buffers + s
         tokens.append(buffers)
         return [t.strip() for t in tokens]  
-
-
-    def split_tokens(self,expression = None):
-        # Split the tokens for evaluation  
-        expression = self._get_expression(expression)
-        tokens = self.tokenize(expression) 
-        parsed = []
-        temp = []
-        for tok in tokens:
-            if len(temp) == 3:
-                parsed.append(temp)
-                temp = []
-                temp.append(tok)
-            elif len(temp) == 1 and temp[0] in self._d.keys():
-                parsed.append(temp)
-                temp = []
-                temp.append(tok)
-            else:
-                temp.append(tok)
-        parsed.append(temp)        
-        return parsed
-            
   
     def evaluate(self,expression = None):
         # evaluate the expression        
         expression = self._get_expression(expression)
-        tokens = self.split_tokens(self.tokenize(expression))
+        tokens = self.tokenize(expression)
+        result = self._df[tokens[0]]
         
-        def evl(x):
-            if len(x) == 3:
-                a = _convert(self._df, x[0])
-                b = _convert(self._df, x[2])
-                return self._d[x[1]](a,b)
-            elif len(x) == 1 and x[0] in self._d.keys():
-                return x
+        for (i,op) in enumerate(tokens[1::2]):
+            c = _convert(self._df, tokens[(i+1)*2])
+            if op in ['<', '>', '<=', '>=', '==', '!=']:
+                result = result[self._d[op](result,c)]
             else:
-                return _convert(self._df,x[0])
-                
-        parsed_tokens = tokens[:]
-                
-        from time import time
-        TIME = time()
+                result = self._d[op](result,c)
+        return result
         
-        while _is_reduced(parsed_tokens) == False:
-            parsed_tokens = [evl(t) for t in parsed_tokens][:]
-            if time() > TIME + 10:
-                print "Timeout error - Too much involved in the program"
-                break
-                 
-        for (i,tok) in enumerate(parsed_tokens):
-            if type(tok) == list:
-                tok = tok[0]
-                parsed_tokens[i] = tok                   
-                if tok in self._d.keys():
-                    a = self._d[tok](parsed_tokens[i-1], parsed_tokens[i+1])
-                    parsed_tokens[i+1] = a           
-                    
-        return parsed_tokens
+def parse(df, expression = None):
+    # Helper function for the Parser Class
+    p = SimpleParser(df, expression)
+    return p.evaluate()
--- financials/parser/tests/test_parse.py
+++ financials/parser/tests/test_parse.py
@@ -5,14 +5,23 @@ Created on Fri Dec 27 19:18:16 2013
 @author: Administrator
 """
 
-from parse import Parser
-from pandas.io.parsers import read_csv
+from financials.parser.parse import SimpleParser, parse
+from pandas import DataFrame
+from pandas.io.parsers import read_csv, ExcelFile
+from numpy import array
 import unittest
 
-df = read_csv('test_parse.csv')
-p = Parser(df)
+
+df = DataFrame([[1,'X',10,1.2,'foo'],
+                [2,'Y',18,2.4,'bar'],
+                [3,'X',9,0.8,'foo'],
+                [4,'Z',16,0.38,'qux'],
+                [5,'Y',13,0.15,'foo']
+                ], columns = ['first', 'second', 'third', 'fourth', 'fifth'])
+
 
 class TestTokenize(unittest.TestCase):
+    p = SimpleParser(df)
     t = p.tokenize
     # More complex expressions are welcome
     expressions_dict = {
@@ -25,16 +34,26 @@ class TestTokenize(unittest.TestCase):
     def test_tokenize(self):
         for k,v in self.expressions_dict.items():
             self.assertEqual(self.t(k), v)
-
-class TestTokenSplit(unittest.TestCase):
-    t = p.split_tokens
-    # More complex expressions are welcome
-    expressions_dict = {
-                        "a+b": [['a', '+', 'b']],
-                        "a+b>b+c": [['a', '+', 'b'], ['>'], ['b', '+ ', 'c']]
-                        }
-
-class TestParser(unittest.TestCase):
-    pass
-
-    
+            
+class TestEvaluate(unittest.TestCase):
+    # Each solution corresponds to a sheet in the solutions.xls file
+    expressions = {
+                "S1": 'first+third',
+                "S2": 'first+third*fourth',
+                "S3": 'fifth == foo',
+                "S4": 'third > 10 + first * fourth',
+                "S5": 'first >=3'
+                }
+                
+    solutions = {
+                "S1": df['first'] + df['third'],
+                "S2": (df['first'] + df['third']) * df['fourth'],
+                "S3": df[df['fifth'] == 'foo']['fifth'],
+                "S4": (df[df['third'] > 10]['third'] + df['first']) * df['fourth'],
+                "S5": df[df['first'] >= 3]['first']
+                }
+                
+    def test_evaluate(self):
+        for (k,v) in self.expressions.items():
+            self.assertEqual(all(parse(df,v).dropna() == self.solutions[k].dropna()), True, k)
+            # Index and na to be tested
