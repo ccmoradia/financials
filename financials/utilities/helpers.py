9bc89477c05c9eb350f9388fd9ad1e4dfc3eefa4
UD <uberdeveloper001@gmail.com>
2014-01-21 18:26:22 +0530

    Reorganize functions

--- financials/utilities/helpers.py
+++ financials/utilities/helpers.py
@@ -7,7 +7,7 @@ Created on Thu Oct 24 06:53:55 2013
 General helper functions
 """
 
-from pandas import *
+from pandas import DataFrame, Series
 from numpy import array, zeros
 
 def dict_to_dataframe(data, rows = "L", cycle = False):
@@ -74,4 +74,56 @@ def dict_to_dataframe(data, rows = "L", cycle = False):
             df[k] = v  
        
     return df
+    
+def dict_to_list(listdict, keys = None, is_JSON = False, as_dataframe = False):
+    """
+    Given a list of dict and keys, return the value of keys as a list
+    
+    listdict : list of dictionaries
+        In case the object is a JSON string, pass True to the is_JSON argument           
+    keys : list
+        keys for which data is to be extracted
+        If None, keys in the first item of the list if picked by default
+    is_JSON: boolean, default False
+        True if the input is a JSON string    
+    as_dataframe : boolean, default False
+        if True, a pandas dataframe is returned with keys as headers
+        
+    >>> list_of_dicts = [{"A": 4, "B": 5}, {"A": 40, "B": 50}]
+    >>> R = [[4,5],[40,50]]
+    >>> R == dict_to_list(list_of_dicts)
+    True
+    >>> import json
+    >>> s = json.dumps(list_of_dicts)
+    >>> R == dict_to_list(s, is_JSON = True)
+    True
+    >>> R = [[4],[40]]
+    >>> R == dict_to_list(list_of_dicts, keys = ["A"])
+    True
+    >>> from pandas import DataFrame
+    >>> df = DataFrame(list_of_dicts)
+    >>> all(df == dict_to_list(list_of_dicts,as_dataframe = True))
+    True
+    >>> D = dict_to_list(s, keys = ["B"], is_JSON = True, as_dataframe = True)
+    >>> del df["A"]
+    >>> all (df == D)
+    True
+    
+    TO DO
+        Deal with missing keys
+        Deal with missing values
+        Deal with error on more than one key
+    """
+    
+    if is_JSON:
+        import json
+        listdict = json.loads(listdict)
+       
+    keys = listdict[0].keys() if keys == None else keys 
+    result = [[k.get(v) for v in keys] for k in listdict]
+    
+    if as_dataframe:
+        return DataFrame(result, columns = keys)
+    else:
+        return result
 
