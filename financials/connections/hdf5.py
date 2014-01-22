c3682f7c6ccc57c880df89793a10ca7980b24a12
UD <uberdeveloper001@gmail.com>
2014-01-22 08:18:24 +0530

    modules hdf5 and text updated to load and fetch data

--- financials/connections/hdf5.py
+++ financials/connections/hdf5.py
@@ -1,6 +1,7 @@
 # Connect from and to HDF5 class
 
 import h5py
+from numpy import dtype
 
 class HDF5Connection(object):
     """
@@ -17,5 +18,82 @@ class HDF5Connection(object):
         **options
         any keyword arguments that could be passed to h5py.File object
         """
-        f = h5py.File(filename, **options)
+        self._f = h5py.File(filename, **options)
+        
+    def __del__(self):
+        # Close the HDF5 file
+        self._f.close()  
+ 
+        
+    def add_data(self, data, path = 'DATA', dtypes = None, convert_object = True, **kwargs):
+        """
+        Add data to an existing HDF5 file. Create the file if it doesn't exist
+        Objects are converted into strings. Datasets created are limited to the
+        maximum shape of 1 billion rows in case of record arrays. Existing datasets
+        are assumed to be flexible so that new data can be added.
+                
+        data: numpy recarray        
+        
+        path: string/ default DATA
+            HDF5 path to write to
+            
+        dtypes: numpy dtype
+            converted to this dtypes when writing to HDF5 file
+            
+        convert_object: Boolean/ default True
+            convert object into strings of size 100. This overrides the dtype option for objects.
+            Pass False to preserve the dtypes
+            
+        kwargs
+        =======
+        List of arguments that could be passed to HDF5 file object
+        
+        maxshape: int/default 1000000000
+            maximum rows of the datasets     
+        
+        """
+        if kwargs.get('maxshape') is None:
+            maxshape = 1000000000
+        else:
+            maxshape = kwargs['maxshape']
+        
+        if convert_object:
+            dtypes = [(k, dtype('S100') if v == dtype('O') else v) for (k,(v,v1)) in data.dtype.fields.items()]
+        else:
+            pass            
+            
+        if dtypes is None:
+            pass
+        else:
+            data = data.astype(dtypes)
+        
+        f = self._f
+        if f.get(path):
+            dset = f[path]
+            l,_ = data.shape
+            d,_ = dset.shape
+            dset.resize((l+d,))
+            dset[d:] = data
+        else:
+            dset = f.create_dataset(path, data = data, maxshape = (maxshape,))
+        
+    def get_data(self, symbols, path = "DATA", **kwargs):
+        """
+        Get data from a HDF5 file
+        
+        symbols: list
+            symbols to get data
+            
+        path: string
+            HDF5 path to extract data
+            
+        kwargs
+        ======
+         
+            
+        """
+        f = self._f
+        dset = f[path]
+        return dset[dset['SYMBOL'] == symbols]
+        
 
--- financials/connections/text.py
+++ financials/connections/text.py
@@ -2,14 +2,14 @@
 
 import os
 from pandas.io.parsers import read_csv
-from pandas import DataFrame, Panel
+from pandas import DataFrame, Panel, concat
 
 class TextConnection(object):
     """
     Text Connection object
     """
     
-    def __init__(self, directory, **options):
+    def __init__(self, directory):
         """
         Directory where file exists
         
@@ -18,14 +18,31 @@ class TextConnection(object):
         self._dir = directory
         
         
-    def aggregate_data(self):
+    def aggregate_data(self, fns = False, **kwargs):
         """
-        Aggregate data into a dataframe by reading file by file
+        Given a directory path, aggregate data from csv files into a dataframe 
+        
+        fns (filename as symbol): Boolean/ default False
+            adds the filename as the symbol column in the dataframe
+        
+        kwargs: Could pass all options to the pandas read_csv function
         """
         df = DataFrame()
-        for (a,b,c) in os.walk(self._dir):
-            for f in c:
-                fn = os.path.join(a,f)
-                df = concat([df, read_csv(fn, **options)])
-                
-        return df
+        for root,dirs,files in os.walk(self._dir):
+            for f in files:
+                fn = os.path.join(root, f)
+                d = read_csv(fn, **kwargs)
+                d['SYMBOL'] = f[:-4]
+                df = concat([df, d])
+        self._df  = df                
+         
+    def get_data(self, symbols, **kwargs):
+        """
+        Get data for the required symbols
+        
+        symbols: string/list
+            list of symbols
+            TO DO: list of symbols to be implemented
+        """
+        df = self._df
+        return df[df['SYMBOL'] == symbols]
