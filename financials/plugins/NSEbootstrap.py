be9483f4b84a6556e658a983e8c40c617794c7ab
UD <uberdeveloper001@gmail.com>
2014-07-27 22:28:44 +0530

    Bootstrap module rewritten from scratch

--- financials/plugins/NSEbootstrap.py
+++ financials/plugins/NSEbootstrap.py
@@ -1,143 +1,76 @@
 import os
 import datetime
-import zipfile
-import tables
-import requests
-import numpy as np
-from pandas import bdate_range, read_csv
-          
-data_type = np.dtype([
-            ("SYMBOL", "S20"),
-            ("SERIES", "S4"),
-            ("OPEN", np.float64),
-            ("HIGH", np.float64),
-            ("LOW", np.float64),
-            ("CLOSE", np.float64),
-            ("LAST", np.float64),
-            ("PREV", np.float64),
-            ("QTY", np.float64),
-            ("VOL", np.float64),
-            ("TS", "S16"),
-            ("TRADES", np.float64),
-            ("ISIN", "S20")
-            ])            
-   
-class BootStrapNSE(object):
+import json
+import sys
+if "/home/ubermensch/financials" not in sys.path:
+    sys.path.append("/home/ubermensch/financials")
+from pandas import DataFrame
+from financials.utilities.bootstrap import BootStrap
+from tables import IsDescription, StringCol, Float16Col, Float32Col, Int32Col
+
+class Equity(IsDescription):
+    SYMBOL = StringCol(50)
+    SERIES = StringCol(2)
+    OPEN = Float32Col()
+    HIGH = Float32Col()
+    LOW = Float32Col()
+    CLOSE = Float32Col()
+    LAST = Float32Col()
+    PREVCLOSE = Float32Col()
+    TOTTRDQTY = Int32Col()
+    TOTTRDVAL = Float32Col()
+    TIMESTAMP = StringCol(12)
+    TOTALTRADES = Int32Col()
+    ISIN = StringCol(12)
+
+class Preopen(IsDescription):
+    symbol = StringCol(50)
+    xDt = StringCol(50)
+    caAct = StringCol(50)
+    iep = Float32Col()
+    chn = Float16Col()
+    perChn = Float16Col()
+    pCls = Float32Col()
+    trdQnty = Int32Col()
+    iVal = Float16Col()
+    sumVal = Float32Col()
+    sumQnty = Int32Col()
+    finQnty = Int32Col()
+    sumfinQnty = Int32Col()
+
+class NSEBootStrap(BootStrap):
     """
-    Bootstrap for NSE
+    BootStrap for NSE
     """
-    def __init__(self, filename, repository = None, schema = data_type, url = None):
+    def url_equity(self, day = datetime.date.today()):
         """
-        Initialize the HDF5 database
-        
-        filename: name of the HDF5 file
-        repository: repository to look for zip files
-        schema: underlying schema of the data
-        url: url to download new data
+        Get the url for equity
         """
-        self._filename = filename
-        self._repository = repository
-        self._schema = data_type
-        self._url = url
-        self._log = []
-         
-    def _download(self, url):
-        """
-        Download the specified url
-        url: the url to download
-        returns True if the url is successfully downloaded 
-        otherwise False
-        """
-        base_url = "http://nseindia.com/content/historical/EQUITIES/"        
-        url = base_url + url
-        try:
-            req = requests.get(url)
-            if req.status_code == 200:
-                with open(os.path.join(self._repository, url.split("/")[-1]), "wb") as f:
-                    f.write(req.content)
-                    req.close()
-                    return True
-            else:
-                req.close()
-                return False
-        except Exception as E:
-            self._log.append(("download", url, E.message))
-            return False
-            
-    def _update_database(self, filename):
-        """
-        update the HDF5 database
-        """
-        if os.path.exists(self._filename):
-            archive = zipfile.ZipFile(filename)
-            fn = archive.namelist()[0] # Reads only the first file from archive
-            df = read_csv(archive.open(fn))
-            # For legacy purpose
-            if len(df.columns) < len(self._schema):
-                for n in range(len(self._schema) - len(df.columns)):
-                    df[str(n)] = np.nan
-            dbase = tables.open_file(self._filename, "a")
-            table = dbase.root.RAW.bhavcopy # This is hardcoded
-            try:
-                table.append(df.values[:,:13].tolist()) 
-                table.flush()
-                dbase.close()
-            except Exception as E:
-                self._log.append(("_update_database", fn, E.message))
-                dbase.close()
-        else:
-            return "File not found"
-        
-    def bootstrap(self, build = False):
-        """
-        Bootstrap with a new HDF5 file
-        
-        build: Boolean
-        If build is True and repository is not None, builds
-        the new database from data in the repository
-        default: False
-        """
-        if os.path.exists(self._filename):
-            return "File already exists"
-        else:
-            dbase = tables.open_file(self._filename, "w")
-            root = dbase.root
-            group = dbase.create_group(root, "RAW")
-            table = dbase.create_table(group, "bhavcopy", self._schema, "NSE Daily Bhav copy of equities market")
-            dbase.close()
-        if build:
-            for root,directory,files in os.walk(self._repository):
-                for f in files:                    
-                    try:
-                        self._update_database(os.path.join(root,f))
-                    except Exception as E:
-                        self._log.append(("bootstrap", f, E.message))
-       
-    def update(self, from_date = "2011-01-01", updateDB = True):
-        """
-        Downloads data, saves in the repository and updates the HDF5 database
-        
-        from_date: date from which update is to be considered
-        updateDB: Boolean
-           If True, update the HDF5 database
-        """
-        if os.path.exists(self._filename):
-            dates = bdate_range(from_date, datetime.date.today())
-            for t in dates:
-                fn = os.path.join(self._repository, "cm" + t.strftime("%d%b%Y").upper() + "bhav.csv.zip")
-                if os.path.exists(fn):
-                    pass
-                else:
-                    status = self._download(t.strftime("%Y/%b/").upper() + "cm" + t.strftime("%d%b%Y").upper() + "bhav.csv.zip")
-                    if updateDB:
-                        if status:
-                            self._update_database(fn)        
-        else:
-            return "File not found"            
-            
-     @property
-     def get_log(self):
-        """
-        Get the error log
-        """
-        return self._log
+        domain = "http://nseindia.com/content/historical/EQUITIES"
+        return self.build_url(domain, day.strftime("%Y"), day.strftime("%b").upper(),
+                              "cm" + day.strftime("%d%b%Y").upper() + "bhav.csv.zip")
+
+    def url_preopen(self):
+        return "http://nseindia.com/live_market/dynaContent/live_analysis/pre_open/nifty.json"
+
+    def filename_preopen(self):
+        return datetime.date.today().strftime("%Y-%m-%d") + ".json"
+
+    def upload_preopen(self, filename):
+        with open(filename, "r") as f:
+            df = DataFrame(json.load(f)["data"])
+        df = df.replace(",", "", regex = True)
+        self.default_upload(df, "preopen")
+
+if __name__ == "__main__":
+    rep = "/home/ubermensch/NSE/data"
+    db = "/home/ubermensch/NSE/test.h5"
+    bt = NSEBootStrap(db, rep)
+    bt.add_path("preopen", "preopen")
+    bt.add_path("bhavcopy", "equity")
+    bt.add_schema("preopen", schema = Preopen, url = bt.url_preopen,
+                filename = bt.filename_preopen, upload = bt.upload_preopen)
+    bt.add_schema("equity", schema = Equity, url = bt.url_equity)
+    bt.update()
+    print bt.log
+
--- financials/utilities/bootstrap.py
+++ financials/utilities/bootstrap.py
@@ -1,70 +1,380 @@
-# Load files into HDF5 archive
-from pandas import *
+"""
+Bootstrap to get started
+"""
+import os
 import zipfile
+import requests
+from tables import open_file, IsDescription, StringCol, BoolCol
+from pandas import read_csv, read_json
+
+run = lambda obj: obj() if hasattr(obj, "__call__") else obj
 
 
-columns = ["SYMBOL", "SERIES", "OPEN", "HIGH", "LOW", "CLOSE",
-           "LAST", "PREVCLOSE", "TOTTRDQTY", "TOTTRDVAL",
-           "TIMESTAMP", "TOTALTRADES", "ISIN"]
-           
-dtypes = [("SYMBOL", "S50"), ("SERIES", "S6"), ("OPEN", "f8"),
-          ("HIGH", "f8"), ("LOW", "f8"), ("CLOSE", "f8"),
-          ("LAST", "f8"), ("PREVCLOSE", "f8"), ("TOTTRDQTY", "f8"),
-          ("TOTTRDVAL", "f8"), ("TIMESTAMP", "S15"),
-          ("TOTALTRADES", "f8"), ("ISIN", "S20")]
 
+class BootStrap(object):
+    def __init__(self, filename, repository, schema = {}):
+        """
+        Bootstrap with a HDF5 database
+        filename
+            name of the HDF5 file
+            If the file doesn't exist, use createDB to create the file
+        repository
+            the directory that holds the files
+        schemas
+            schema as dict/json file
+        """
+        self._DB = filename
+        self._repository = repository
+        self._schema = schema
+        self._log = []
 
-def zip_to_dataframe(path, filename, headers = columns):
-    # path must include the final slash /
-    # convert a csv file in zip archive to dataframe
-    # headers - default headers to create a dataframe
-    with zipfile.ZipFile(path + filename) as z:        
-        df = read_csv(z.open(filename[:-4])).drop_duplicates()
-        df = df.drop(df[df.SYMBOL == "SYMBOL"].index).iloc[:,:-1]
-        return concat([DataFrame(columns = headers), df])
-
-def concat_dataframes(path, files, headers = columns):
-    # unzips files, reads csv and joins them into a single dataframe
-    # headers - default headers to create a dataframe    
-    df = DataFrame(columns = headers) # Empty dataframe with required columns
-    return concat([df, concat([zip_to_dataframe(path, f) for f in files])])
-    
-def convert_dataframe_to_recarray(dataframe):
-    # helper functions to convert dataframe to recarray
-    # main function is to convert objects into strings
-    rec = dataframe.to_records(index = False)
-    return rec.astype(dtype = dtypes)
-
-def write_recarray_to_hdf5(recarray, filename, key = "RAW"):
-    # write recarray to a HDF5 file. Uses vanilla h5py
-    # writes data straight under root
-    import h5py
-    f = h5py.File(filename)
-    if f.get(key):
-        dset = f[key]
-        # print 'Existing dset shape - ', dset.shape
-        l, = recarray.shape
-        d, = dset.shape
-        dset.resize((l+d,))
-        dset[d:] = recarray
-        # print 'New dset shape - ', dset.shape        
-    else:
-        dset = f.create_dataset(key, data = recarray,maxshape = (1000000000,))
-    f.close()
-    
-def run_bootstrap():
-    # Bootstrap to load initial data - crappy code
-    filelist = read_csv('filelist_2011.csv').values
-    filelist = filelist.ravel()
-    path = "G:/Projects/finance/NSE/"
-    for (y,x) in enumerate(filelist):
+    def _download(self, url):
+        """
+        Download the url
+        Returns the request content
+        else returns False
+        """
+        url = run(url)
         try:
-            df = zip_to_dataframe(path, x)
-            rec = convert_dataframe_to_recarray(df)
-            write_recarray_to_hdf5(rec, 'data.h5','FROM2011')
-            # print "Success ", x
-        except Exception as inst:
-            print y, type(inst), x
-            
-if __name__ == "__main__":
-    run_bootstrap()
+            req = requests.get(url)
+            if req.status_code == 200:
+                return req
+            else:
+                req.close()
+                self._log.append(("_download", url, req.status_code))
+                return False
+        except Exception as E:
+            self._log.append(("_download", url, E.message))
+            return False
+
+    def _write_to_repository(self, req, path, filename = None):
+        """
+        Write the content to repository
+        returns the filename if file succesfully written
+        else false
+        req
+            A url request object from the _download function
+        path
+            repository path
+        """
+        try:
+            if req:
+                if filename is None:
+                    filename = req.url.split("/")[-1]
+                elif hasattr(filename, "__call__"):
+                    filename = filename()
+                else:
+                    filename
+
+                pth = os.path.join(path, filename)
+                if os.path.exists(pth):
+                    self._log.append(("_write_to_repository", path, "File already exists"))
+                    return False
+                else:
+                    with open(pth, "wb") as f:
+                        f.write(req.content)
+                        req.close()
+                        return pth
+            else:
+                return False
+        except Exception as E:
+            self._log.append(("_write_to_repository", path, E.message))
+
+    def _upload_to_DB(self, filename, name, n_check = 10, **kwargs):
+        """
+        Uploads a file to DB
+        filename
+            filename to upload
+        name
+            The name of the schema
+        n_check
+            The number of last files to check whether
+            they are already uploaded to DB
+        """
+        with open_file(self._DB, "r+") as h5file:
+            desc = self.get_schema(name)
+            sync = h5file.get_node(h5file.root._SYNC, desc["path"]) \
+                   [-n_check:]["filename"]
+        if filename.split("/")[-1] in sync:
+            self._log.append(("_upload_to_DB", filename, "File already uploaded to DB"))
+            return False
+
+        if self.get_schema(name)["upload"] is None:
+            _,ext = os.path.splitext(filename)
+            if ext in [".gzip", ".bz2"]:
+                df = read_csv(filename, compression = ext)
+            elif ext == ".zip":
+                z = zipfile.ZipFile(filename)
+                df = read_csv(z.open(z.namelist()[0]))
+            else:
+                pass
+            self.default_upload(df, name)
+        else:
+            self.get_schema(name)["upload"](filename = filename, **kwargs)
+
+        with open_file(self._DB, "r+") as h5file:
+            desc = self.get_schema(name)
+            sync_table = h5file.get_node(h5file.root._SYNC, desc["path"])
+            it = sync_table.row
+            for i in range(2):
+                it["filename"] = filename.split("/")[-1]
+                it["flag"] = True
+            it.append()
+            sync_table.flush()
+        return True
+
+    def _run_before(self, before, schema, **kwargs):
+        """
+        function to run before an action
+        before
+            string. any of url, filename, upload, sync
+        schema
+            schema as dict
+        """
+        if schema.get("before") is not None:
+            for a,func in schema.get("before"):
+                if a == before:
+                    func(**kwargs)
+
+    def _run_after(self, after, schema, **kwargs):
+        """
+        function to run after an action
+        after
+            string. any of url, filename, upload, sync
+        schema
+            schema as dict
+        """
+        if schema.get("after") is not None:
+            for a,func in schema.get("after"):
+                if a == after:
+                    func(**kwargs)
+
+    @property
+    def log(self):
+        return self._log
+
+    @property
+    def repository(self):
+        return self._repository
+
+    @property
+    def db_name(self):
+        return self._DB
+
+    def createDB(self, create_tables = False, force_overwrite = False):
+        """
+        create the HDF5 file if it doesn't exist
+        create_tables
+            If True all tables are created with parent nodes
+        force_overwrite
+            Overwrite an existing file with this file
+            default: False
+        """
+        if os.path.exists(self._DB):
+            if force_overwrite:
+                h5file = open_file(self._DB, "w")
+            else:
+                return "File already exists"
+        if create_tables:
+            DATA = h5file.create_group("/", "_DATA")
+            SYNC = h5file.create_group("/", "_SYNC")
+            class Sync(IsDescription):
+                filename = StringCol(60)
+                flag = BoolCol()
+            for k,v in self._schema.iteritems():
+                h5file.create_table(where = DATA, name = v["path"], description = v["schema"])
+                h5file.create_table(where = SYNC, name = v["path"], description = Sync)
+        h5file.close()
+
+
+    def add_path(self, path, name = None):
+        """
+        Add path to the existing HDF5 file
+        This adds a node to the HDF5 file
+        path
+            HDF5 path. String
+        name
+            A custom name/description for the path
+            Don't use / in your name
+        """
+        if name is None:
+            name = path.split("/")[-1]
+        self._schema[name] = {}
+        self._schema[name]["path"] = path
+
+    def add_schema(self, name, schema, url, filename = None, upload = None,
+                   sync = None, before = None, after = None):
+        """
+        The schema for the specified path. All arguments are functions except name
+        name
+            name/path of the node. If there is a / in the argument, it
+            is considered a path otherwise a node. The node must exist
+        schema
+            A description of the data in an accepted PyTables format.
+            Any valid description that support the create_table method in
+            PyTables
+        url
+            A function, probably the build_url, that returns a url as a string
+        filename
+            Filename to be saved in the local repository.
+            If None, the last string from the url is used as the filename
+        upload
+            function to upload the data from the repository to the HDF5 file.
+            By default, the file is read line by line and appended to the HDF5 file.
+            It is assumed that the file is in csv format
+        sync
+            A function to synchronize the data in the repository and the HDF5 file
+            By default, it is assumed that all data is written to the HDF5 file
+            immediately after a file is downloaded and saved in the repository
+        before
+            Functions to be run before any of url,filename,transform or sync
+            A list of 2-tuples with the first being a string with one of the values
+            url, filename, transform or sync and the second being the function
+            to run
+        after
+            Similar to before but run after any of url,filename,transform or sync
+            is executed
+        Notes
+        =====
+        The functions before and after are run in the following order.
+        For url, it is run before the url request is sent and after the url
+        request is received
+        For filename, before the file is downloaded from the url and after the
+        url is downloaded
+        For upload, before the file is read and any transformation is made and
+        after the file is uploaded to the HDF5 database.
+        For sync, before and after the sync function is run.
+        """
+        if "/" in name:
+            for k,v in self._schema.items():
+                if v[path] == name:
+                    name = k
+                    break
+
+        desc = self._schema[name]
+        desc["schema"] = schema
+        desc["url"] = url
+        desc["filename"] = filename
+        desc["upload"] = upload
+        desc["sync"] = sync
+        desc["before"] = before
+        desc["after"] = after
+
+
+    def default_upload(self, dataframe, name):
+        """
+        The default upload method
+        Given a dataframe and a schema name, uploads data to DB
+        for columns found in both dataframe and schmea
+        """
+        with open_file(self._DB, "r+") as h5file:
+            desc = self.get_schema(name)
+            data_table = h5file.get_node(h5file.root._DATA, desc["path"])
+            it = data_table.row
+            for i in range(len(dataframe)):
+                for col in data_table.colnames:
+                    it[col] = dataframe.at[i,col]
+                it.append()
+            data_table.flush()
+
+    def load_schema(self, schema):
+        """
+        Load schema from a dictionary or a json file
+        Overwrites the existing schema
+        """
+        if type(schema) == dict:
+            self._schema = schema
+        else:
+            import json
+            with open(schema, "r") as f:
+                self._schema = json.load(f)
+
+    def get_schema(self, name = None):
+        """
+        returns the existing schema
+        name
+            name of the the schema
+        """
+        return self._schema if name is None else self._schema.get(name)
+
+    def build_url(self, domain, *args):
+        """
+        Build an url to download data
+        domain
+            the domain of the website
+        args
+            keyword args that specify different parts of the url
+            If the args is a function it must return a value
+        """
+        params = "/".join([str(x) for x in args])
+        return domain + "/" + params
+
+    def update_repository(self, *args):
+        """
+        Download the files from the url and save them in the repository
+        By default, all urls are downloaded
+        To download only specific urls, pass the names as arguments
+        """
+        if len(args) == 0:
+            args = self._schema.keys()
+        for k in args:
+            v = self._schema.get(k)
+            self._run_before("url", v)
+            req = self._download(v["url"])
+            self._run_after("url", v)
+
+            self._run_before("filename", v)
+            self._write_to_repository(req, os.path.join(self._repository, v["path"]), v["filename"])
+            self._run_after("filename", v)
+
+    def update(self, *args):
+        """
+        Download the files from the url, save them in the repository and
+        upload to the HDF5 file
+        By default, all urls are downloaded
+        To upload only specific files, pass the names as arguments
+        """
+        if len(args) == 0:
+            args = self._schema.keys()
+        for k in args:
+            v = self.get_schema(k)
+            self._run_before("url", v)
+            req = self._download(v["url"])
+            self._run_after("url", v)
+
+            path = os.path.join(self._repository, v["path"])
+            self._run_before("filename", v)
+            filename = self._write_to_repository(req, path, v["filename"])
+            self._run_after("filename", v)
+
+            if filename:
+                self._run_before("upload", v)
+                self._upload_to_DB(os.path.join(path, filename), k)
+                self._run_after("upload", v)
+            else:
+                self._log.append(("update", filename, "File not uploaded"))
+
+    def syncDB(self, *args):
+        """
+        Synchronize the repository with the HDF5 file
+        This function iterates the repository and uploads file to the DB
+        that are earlier not uploaded. This ischs useful when downloads are done
+        at one time and uploads at another time
+        Names as args
+        """
+        if len(args) == 0:
+            args = self._schema.keys()
+        for k in args:
+            v = self._schema.get(k)
+            self._run_before("sync", v)
+            if v["sync"] is None:
+                with open_file(self._DB, "r+") as h5file:
+                    path = os.path.join(self._repository, v["path"])
+                    for dirpath, dirnames, filenames in os.walk(path):
+                        filelist = h5file.get_node(h5file.root._SYNC, v["path"]).read(field = "filename")
+                        for f in filenames:
+                            if f not in filelist:
+                                self._upload_to_DB(os.path.join(path, f), k)
+            else:
+                v["sync"]()
+                self._run_after("sync", v)
