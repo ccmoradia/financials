c8b43f65b05e44bf1be58ce67117cb75ee0088b5
UD <uberdeveloper001@gmail.com>
2014-07-28 21:43:21 +0530

    Derivative class added to NSEbootstrap

--- financials/plugins/NSEbootstrap.py
+++ financials/plugins/NSEbootstrap.py
@@ -6,7 +6,8 @@ if "/home/ubermensch/financials" not in sys.path:
     sys.path.append("/home/ubermensch/financials")
 from pandas import DataFrame
 from financials.utilities.bootstrap import BootStrap
-from tables import IsDescription, StringCol, Float16Col, Float32Col, Int32Col
+from tables import IsDescription, StringCol, Float16Col, Float32Col,\
+                   Float64Col, Int32Col
 
 class Equity(IsDescription):
     SYMBOL = StringCol(50)
@@ -18,7 +19,7 @@ class Equity(IsDescription):
     LAST = Float32Col()
     PREVCLOSE = Float32Col()
     TOTTRDQTY = Int32Col()
-    TOTTRDVAL = Float32Col()
+    TOTTRDVAL = Float64Col()
     TIMESTAMP = StringCol(12)
     TOTALTRADES = Int32Col()
     ISIN = StringCol(12)
@@ -38,6 +39,24 @@ class Preopen(IsDescription):
     finQnty = Int32Col()
     sumfinQnty = Int32Col()
 
+class Derivative(IsDescription):
+    INSTRUMENT = StringCol(6)
+    SYMBOL = StringCol(50)
+    EXPIRY_DT = StringCol(11)
+    STRIKE_PR = Int32Col()
+    OPTION_TYP = StringCol(2)
+    OPEN = Float32Col()
+    HIGH = Float32Col()
+    LOW = Float32Col()
+    CLOSE = Float32Col()
+    SETTLE_PR = Float32Col()
+    CONTRACTS = Int32Col()
+    VAL_INLAKH = Float64Col()
+    OPEN_INT = Int32Col()
+    CHG_IN_OI = Int32Col()
+    TIMESTAMP = StringCol(11)
+
+
 class NSEBootStrap(BootStrap):
     """
     BootStrap for NSE
@@ -49,7 +68,6 @@ class NSEBootStrap(BootStrap):
         domain = "http://nseindia.com/content/historical/EQUITIES"
         return self.build_url(domain, day.strftime("%Y"), day.strftime("%b").upper(),
                               "cm" + day.strftime("%d%b%Y").upper() + "bhav.csv.zip")
-
     def url_preopen(self):
         return "http://nseindia.com/live_market/dynaContent/live_analysis/pre_open/nifty.json"
 
@@ -62,15 +80,22 @@ class NSEBootStrap(BootStrap):
         df = df.replace(",", "", regex = True)
         self.default_upload(df, "preopen")
 
+    def url_derivative(self, day = datetime.date.today()):
+        domain = "http://nseindia.com/content/historical/DERIVATIVES"
+        return self.build_url(domain, day.strftime("%Y"), day.strftime("%b").upper(),
+                              "fo" + day.strftime("%d%b%Y").upper() + "bhav.csv.zip")
+
+
 if __name__ == "__main__":
     rep = "/home/ubermensch/NSE/data"
     db = "/home/ubermensch/NSE/test.h5"
     bt = NSEBootStrap(db, rep)
     bt.add_path("preopen", "preopen")
     bt.add_path("bhavcopy", "equity")
+    bt.add_path("fo", "derivative")
     bt.add_schema("preopen", schema = Preopen, url = bt.url_preopen,
                 filename = bt.filename_preopen, upload = bt.upload_preopen)
     bt.add_schema("equity", schema = Equity, url = bt.url_equity)
-    bt.update()
+    bt.add_schema("derivative", schema = Derivative, url = bt.url_derivative)
+    bt.update("equity", "preopen")
     print bt.log
-
