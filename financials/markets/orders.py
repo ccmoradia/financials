91f1ecd9ef65ce21f38988977b3115bfe5481ac2
UD <uberdeveloper001@gmail.com>
2014-08-14 07:22:39 +0530

    Tests rewritten

--- financials/markets/orders.py
+++ financials/markets/orders.py
@@ -1,42 +1,44 @@
-from pandas import *
+from pandas import Series, DataFrame
 from numpy import array, zeros
+from collections import Iterable
+
 def stoploss(data,percentage,price_column,order_column,**kwargs):
     """
     Given order data and a percentage, create a stop loss order
-    
+
     data - DataFrame containing order information
-    
+
     percentage - the percentage of stop loss. Provide a list in case of different
     percentages for different orders. Given a numpy array, the length of the list
     must match with the number of orders. Also, the list must be sorted to
     match the orders
-    
+
     price_column - column that contains price on which the stop loss percentage
     is calculated
-    
+
     order_column - column that contains the order type BUY/SELL. If orders have
-    different codes other than BUY/SELL, use kwargs    
-    
-    
-    **kwargs**    
-    
+    different codes other than BUY/SELL, use kwargs
+
+
+    **kwargs**
+
     Pair - list - keyword for BUY,SELL
-    
+
     ORDERCODE_COL - string - column that contains the order code
-    
+
     STOPLOSS_CODE - string - code for the Stop Loss order to be filled up
                              in ORDERTYPE_COL
-     
+
     Notes:
     Any value that matches a DataFrame Column can be given as an argument to
     kwargs. Given a column name as key and a matching value, the default
     behavior of this function is to replace the entire column with the given
-    value.    
-    
+    value.
+
     StopLoss calculation
      * Long orders = price * (1 + percentage)
      * Short orders = price * (1 - percentage)
-    
+
     >>> df = DataFrame([list("ABCD"), list("WXYZ"),range(1,5),["BUY","SELL","BUY","SELL"]]).transpose()
     >>> df.columns = ["Symbol", "Remarks", "Price", "OrderType"]
     >>> df2 = df.copy()
@@ -50,15 +52,18 @@ def stoploss(data,percentage,price_column,order_column,**kwargs):
     >>> df2["OrderType"] = ["S", "B", "S", "B"]
     >>> all(df2 == stoploss(df,1,"Price","OrderType", Pair = ["B", "S"]))
     True
-    
+
     """
     if kwargs.get("Pair"):
         Pair = kwargs["Pair"]
         s = Series({Pair[0]:Pair[1], Pair[1]:Pair[0]})
     else:
-        s = Series({"BUY": "SELL", "SELL": "BUY"})     
+        s = Series({"BUY": "SELL", "SELL": "BUY"})
     p = percentage * 0.01
-    SL = [price *(1-p) if x==s[1] else price*(1+p) for (price,x) in zip(data[price_column], data[order_column])]
+    if not isinstance(p, Iterable):
+        p = [p] * len(data)
+    SL = [price *(1-per) if x==s[1] else price*(1+per) for (price,x,per) in
+          zip(data[price_column], data[order_column], p)]
     df = data.copy()
     df[price_column] = SL
     df[order_column] = df[order_column].map(s)
--- financials/markets/tests/test_Portfolio.py
+++ financials/markets/tests/test_Portfolio.py
@@ -1,23 +1,23 @@
-from financials.markets.Portfolio import Portfolio
 import unittest
+import datetime
+
+from financials.markets.Portfolio import Portfolio
+from pandas.io.parsers import read_csv
 
 class TestPortfolio(unittest.TestCase):
 
     pf = Portfolio()
-       
+
     def test_funds(self):
         # test addition and withdrawal of funds
         self.pf.add_funds(2000)
         self.pf.add_funds(3000)
-        self.assertEqual(self.pf.balance(), 5000)
+        self.assertEqual(self.pf.cash_balance, 5000)
         self.pf.add_funds(-1000)
-        self.assertEqual(self.pf.balance(), 6000)
+        self.assertEqual(self.pf.cash_balance, 6000)
         self.pf.withdraw_funds(1000)
         self.pf.withdraw_funds(1500)
-        self.assertEqual(self.pf.balance(), 3500)
-        self.pf.add_funds(['2014-01-02',2000])
-        self.pf.withdraw_funds(['2014-01-03', 5500])
-        self.assertEqual(self.pf.balance(),0)
-        self.pf.add_funds(['2014', 1, 10000]) # Test for irregular array shapes
-        self.pf.withdraw_funds(['2014', '01', '10', 5000])
-        self.assertEqual(self.pf.balance(), 5000)
+        self.assertEqual(self.pf.cash_balance, 3500)
+        self.pf.add_funds(2000, TS = '2014-01-02')
+        self.pf.withdraw_funds(5500, TS = '2014-01-03')
+        self.assertEqual(self.pf.cash_balance,0)
