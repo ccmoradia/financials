b43c035fb16ad912bd40022458ee964dd94de7fa
UD <uberdeveloper001@gmail.com>
2014-10-10 07:44:56 +0530

    decorators update

--- financials/utilities/decorators.py
+++ financials/utilities/decorators.py
@@ -2,10 +2,10 @@
 
 from functools import wraps
 
-def select_period(function):
+def period(function):
     """
-    Given a Series/DataFrame and the arguments f,t selects the data between
-    the period f and t.
+    Given a Series/DataFrame with a datetime index and the arguments f,t
+    selects the data between the period f and t including f and t
     f
         from period - valid datetime
     t
@@ -17,19 +17,33 @@ def select_period(function):
     """
     @wraps(function)
     def wrapped(df, *args, **kwargs):
-        df2 = df.loc[kwargs.get("f"): kwargs.get("t")]
-        return func(df2, *args, **kwargs)
+        try:
+            df2 = df.loc[kwargs.get("f"): kwargs.get("t")]
+        except KeyError:
+            raise TypeError("Index not a valid datetime object")
+        kwargs.pop("f", None)
+        kwargs.pop("t", None)
+        return function(df2, *args, **kwargs)
     return wrapped
 
 def frequency(function):
     """
     Groups data by the given frequency
     freq
-        frequency
+        A valid pandas DateOffset object or string
+
+    Notes
+    -----
+    Uses the ffill method to fill nans
+
+    TO DO: add arguments to decorators
     """
     @wraps(function)
     def wrapped(df, *args, **kwargs):
-        freq = kwargs.get("freq")
-        result = func(df2, *args, **kwargs)
-        return result.groupby(freq)
+        if kwargs.get("freq") is not None:
+            df2 = df.asfreq(kwargs.get("freq"), method = "ffill")
+        else:
+            df2 = df
+        kwargs.pop("freq", None)
+        return function(df2, *args, **kwargs)
     return wrapped
